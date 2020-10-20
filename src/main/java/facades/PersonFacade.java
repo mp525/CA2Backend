12a1c680a7aaaf7acd79f2fb0afbda1a9bdefdd0
Sjuber <54933648+Sjuber@users.noreply.github.com>
2020-10-20 10:18:54 +0200

    Rest and other things

--- src/main/java/facades/PersonFacade.java
+++ src/main/java/facades/PersonFacade.java
@@ -17,7 +17,8 @@ import javax.persistence.TypedQuery;
  * @author Mathias
  */
 public class PersonFacade {
-     private static PersonFacade instance;
+
+    private static PersonFacade instance;
     private static EntityManagerFactory emf;
 
     private PersonFacade() {
@@ -30,22 +31,36 @@ public class PersonFacade {
             instance = new PersonFacade();
         }
         return instance;
-}
-    public PersonDTO getByPhone(int phonenr){
-        EntityManager enf= emf.createEntityManager();
+    }
+
+    public PersonDTO getByPhone(int phonenr) {
+        EntityManager enf = emf.createEntityManager();
         TypedQuery<Person> query = enf.createQuery(
-        "SELECT p FROM Phone p INNER JOIN e.person e where e.number"+ phonenr +"", Person.class);
+                "SELECT p FROM Phone p INNER JOIN e.person e where e.number" + phonenr + "", Person.class);
         Person result = query.getSingleResult();
         return new PersonDTO(result);
     }
-    
-    public List<PersonDTO> getAllByHobby(int hobby){
-        EntityManager enf= emf.createEntityManager();
+
+    public List<PersonDTO> getAllByHobby(int hobby) {
+        EntityManager enf = emf.createEntityManager();
         TypedQuery<Person> query = enf.createQuery(
-        "SELECT p FROM Person p INNER JOIN p.hobbies h where h.name"+hobby+"", Person.class);
+                "SELECT p FROM Person p INNER JOIN p.hobbies h where h.name" + hobby + "", Person.class);
         List<Person> result = query.getResultList();
-        PersonDTO pdto= new PersonDTO();
-        List<PersonDTO>listDTO= pdto.toDTO(result);
-    return listDTO;
+        PersonDTO pdto = new PersonDTO();
+        List<PersonDTO> listDTO = pdto.toDTO(result);
+        return listDTO;
+    }
+
+    public PersonDTO editPerson(String firstName, String lastName, String email) {
+        Person person = new Person(firstName, lastName, email);
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(person);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+        return new PersonDTO(person);
     }
 }
