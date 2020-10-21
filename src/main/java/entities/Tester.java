de1facced0c2599e2ee29116a41e40f3ee0358a7
Mathias Parking <cph-mp525@cphbusiness.dk>
2020-10-21 13:53:55 +0200

    tester commented

--- src/main/java/entities/Tester.java
+++ src/main/java/entities/Tester.java
@@ -21,43 +21,14 @@ import utils.EMF_Creator;
 public class Tester {
 
     public static void main(String[] args) throws PersonNotFoundException {
-        EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
-        EntityManager em = emf.createEntityManager();
-
-
-        Person p1 = new Person("email1", "fornavn", "efternavn");
-        Person p2 = new Person("email2", "navn", "navn2");
-        Person p3 = new Person("email3", "navnet", "navnet2");
-        CityInfo cf = new CityInfo("2750","Ballerup");
-
-        Address a1 = new Address("Street", "info");
-        Address a2 = new Address("street2", "info2");
-        Address a3 = new Address("street3", "info3");
-        
-        a1.addPerson(p1);
-        a1.addPerson(p2);
-        a2.addPerson(p3);
-        
-        Phone ph1 = new Phone(11111111, "Home");
-        Phone ph2 = new Phone(11111112, "Home");
-        Phone ph3 = new Phone(11111113, "Home");
-
-        p1.addPhone(ph1);
-        p2.addPhone(ph2);
-        p2.addPhone(ph3);
-        
-        Hobby h1 = new Hobby("name", "wikilink", "categoy", "type");
-        Hobby h2 = new Hobby("dnd", "wikilink", "categoy", "type");
-        
-        p1.addHobby(h1);
-        p1.addHobby(h2);
-        p3.addHobby(h1);
-
-
-
+//        EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
+//        EntityManager em = emf.createEntityManager();
+//
+//
 //        Person p1 = new Person("email1", "fornavn", "efternavn");
 //        Person p2 = new Person("email2", "navn", "navn2");
 //        Person p3 = new Person("email3", "navnet", "navnet2");
+//        CityInfo cf = new CityInfo("2750","Ballerup");
 //
 //        Address a1 = new Address("Street", "info");
 //        Address a2 = new Address("street2", "info2");
@@ -81,31 +52,60 @@ public class Tester {
 //        p1.addHobby(h1);
 //        p1.addHobby(h2);
 //        p3.addHobby(h1);
-
-        //Person p1 = new Person("email1", "fornavn", "efternavn");
-
-//String email, String firstName, String lastName, String street, String houseNr, int zip, String hobbyName
-       // PersonDTO dto1 = new PersonDTO("mail2", "firstName2", "lastName2", "Street2", "houseNr2", "1011", "Animation");
-        PersonDTO dtol2 = new PersonDTO(p1);
-        PersonFacade facade = PersonFacade.getGMPFacade(emf);
-        facade.addPerson(dto1);
-        facade.editPerson(dtol2);
-
-         a1.setCityInfo(cf);
-        a2.setCityInfo(cf);
-        a3.setCityInfo(cf);
-        p1.setAddress(a1);
-        p2.setAddress(a2);
-        p3.setAddress(a3);
-        try {
-            em.getTransaction().begin();
-            em.persist(p1);
-            em.persist(p2);
-            em.persist(p3);
-            em.getTransaction().commit();
-        } finally {
-            em.close();
-        }
+//
+//
+//
+////        Person p1 = new Person("email1", "fornavn", "efternavn");
+////        Person p2 = new Person("email2", "navn", "navn2");
+////        Person p3 = new Person("email3", "navnet", "navnet2");
+////
+////        Address a1 = new Address("Street", "info");
+////        Address a2 = new Address("street2", "info2");
+////        Address a3 = new Address("street3", "info3");
+////        
+////        a1.addPerson(p1);
+////        a1.addPerson(p2);
+////        a2.addPerson(p3);
+////        
+////        Phone ph1 = new Phone(11111111, "Home");
+////        Phone ph2 = new Phone(11111112, "Home");
+////        Phone ph3 = new Phone(11111113, "Home");
+////
+////        p1.addPhone(ph1);
+////        p2.addPhone(ph2);
+////        p2.addPhone(ph3);
+////        
+////        Hobby h1 = new Hobby("name", "wikilink", "categoy", "type");
+////        Hobby h2 = new Hobby("dnd", "wikilink", "categoy", "type");
+////        
+////        p1.addHobby(h1);
+////        p1.addHobby(h2);
+////        p3.addHobby(h1);
+//
+//        //Person p1 = new Person("email1", "fornavn", "efternavn");
+//
+////String email, String firstName, String lastName, String street, String houseNr, int zip, String hobbyName
+//       // PersonDTO dto1 = new PersonDTO("mail2", "firstName2", "lastName2", "Street2", "houseNr2", "1011", "Animation");
+//        PersonDTO dtol2 = new PersonDTO(p1);
+//        PersonFacade facade = PersonFacade.getGMPFacade(emf);
+//        //facade.addPerson(dto1);
+//        facade.editPerson(dtol2);
+//
+//         a1.setCityInfo(cf);
+//        a2.setCityInfo(cf);
+//        a3.setCityInfo(cf);
+//        p1.setAddress(a1);
+//        p2.setAddress(a2);
+//        p3.setAddress(a3);
+//        try {
+//            em.getTransaction().begin();
+//            em.persist(p1);
+//            em.persist(p2);
+//            em.persist(p3);
+//            em.getTransaction().commit();
+//        } finally {
+//            em.close();
+//        }
 
 
         // facade.deletePerson(dto1.getId());
